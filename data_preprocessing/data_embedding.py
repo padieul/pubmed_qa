32b48abb47181a37983efd0cf8c079abfd68181d
Abdulghani Almasri <a-almasri@outlook.com>
2023-12-15 03:16:05 +0300

    Updating the embedding script.

--- data_preprocessing/data_embedding.py
+++ data_preprocessing/data_embedding.py
@@ -1,25 +1,40 @@
-from sentence_transformers import SentenceTransformer
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
-import numpy as np
+import csv
 
-# This script generate 
+
+# Import chunked data
+df = pd.read_csv('Project\\data\\exported_data_with_chunks.csv')
+
+# Drop abstracts with NaN values 
+df = df.dropna(subset=['Abstract'])
 
 # Import the embedding model
 model = SentenceTransformer('all-mpnet-base-v2')
 
-# Read the chunked data
-df_data = pd.read_csv('data_chunks.csv')
 
-# Generate the embeddings for the chunked data
-tqdm.pandas(desc='Generating embeddings')
-df_data['Abstract_emb'] = df_data.progress_apply(lambda row: model.encode(row['Chunk']).tolist(), axis = 1)
+# Embed the chunks between the start and end row (we do embeddings in batches as it task a lot of time)
+start_row = 6000
+end_row = 10000
 
-# Save the chunked data with the embeddings in a CSV file (We save the data in chunks so we can show the progress in tqdm)
-splits = np.array_split(df_data.index, 100)
+rows_list = []
 
-for index, split in enumerate(tqdm(splits, desc='Saving to CSV')):
-    if index == 0:
-        df_data.loc[split].to_csv('data_chunks_emb.csv', mode='w', index=False)
-    else:
-        df_data.loc[split].to_csv('data_chunks_emb.csv', header=None, mode='a', index=False)
+for index, row in tqdm(df.iterrows(), total=end_row, desc='Embedding data'):    
+    
+    if index < start_row:
+        continue
+    if index == end_row:
+        break
+
+    embedding = model.encode(row['Chunk']).tolist()
+
+    rows_list.append([row['PMID'], row['Title'], row['Abstract'], row['Chunk_id'], row['Chunk'], embedding, row['Key_words'], row['Authors'], row['Journal'], row['Year'], row['Month'], row['Source'], row['Country']])
+
+
+df_save = pd.DataFrame(rows_list, columns=["pmid", "title", "abstract", "chunk_id", "chunk", "embedding", "key_words", "authors", "journal", "year", "month", "source", "country"])
+
+
+# Append the data to the existing embeddings file
+# For the first batch use df_save.to_csv('Project\\data\\data_embeddings.csv',index=False)
+df_save.to_csv('Project\\data\\data_embeddings.csv', mode='a', index=False, header=False)
