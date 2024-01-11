38f8d64da0a98a1bea97366b3e6a38f97ca2133a
Sushmitha <52081487+smitha13798@users.noreply.github.com>
2024-01-11 12:04:28 +0100

    Frontend code modified

--- Frontend/index.js
+++ Frontend/index.js
@@ -4,10 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile('C:/Users/sushm/OneDrive/Desktop/Sem1/NLP/publicindex.html', { root: __dirname + '/public' });
+  res.sendFile('index.html', { root: __dirname + '/public' });
 });
 
 app.listen(port, () => {
