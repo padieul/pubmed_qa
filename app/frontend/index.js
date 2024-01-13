c2760dd0a54517b9384e31d8d58ad9700cfc2bc3
Paul Dietze <prdietze@outlook.de>
2024-01-13 16:51:53 +0100

    node.js frontend now works locally

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -1,14 +1,17 @@
 // index.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/public' });
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Current directory: ${__dirname}`);
 });
