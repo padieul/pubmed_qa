aa47889541b6774c6fd134fd6f2799cf6fe44e7e
Paul Dietze <prdietze@outlook.de>
2024-02-08 18:05:24 +0100

    minor experimenting with hf serverless

---
+++ data_preprocessing/test_llama.py
@@ -0,0 +1,17 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
+
+print(output)
+
+
+
